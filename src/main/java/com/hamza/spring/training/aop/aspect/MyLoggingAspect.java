88bcec56b20025c481709c2d0278d4842fce1a7e
Hamza <hamza.dabjan@gmail.com>
2024-09-30 03:30:46 +0300

    Pointcut Declaration

--- src/main/java/com/hamza/spring/training/aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/hamza/spring/training/aop/aspect/MyLoggingAspect.java
@@ -3,6 +3,7 @@ package com.hamza.spring.training.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -13,7 +14,19 @@ public class MyLoggingAspect {
 
     // Let's start with an @Before advice
 
-    @Before("execution( * add*(*,..))")
+    @Pointcut("execution( * add*(*,..))")
+    private void forDaoPackage(){};
+
+    @Pointcut("execution( * get*(..))")
+    private void getter(){};
+
+    @Pointcut("execution( * set*(..))")
+    private void setter(){};
+
+    @Pointcut("forDaoPackage() && !getter() && !setter()")
+    private void forAllMethodsExcludeGetterAndSetter(){};
+
+    @Before("forAllMethodsExcludeGetterAndSetter()")
     public void  beforeAddAccountAdvice() {
         System.out.println("\n ======>>> Executing @Before advice of the addAccount() method.");
     }
